3323d8ab97b032260e12beb027807ac40655ab97
Brandon Green <brandon@brandonleeproductions.com>
2018-08-29 00:25:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,15 +2,17 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
-
     if Time < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
+    end
+end
+
+    # num_1 = Kernel.rand(1..20)
+    # num_2 = Kernel.rand(1..20)
+    # num_3 = Kernel.rand(1..20)
+
   #   if num_1==num_2 && num_2==num_3
   #     resp.write "You Win"
   #   else
@@ -18,6 +20,3 @@ class Application
   #   end
   #
   #   resp.finish
-  # end
-
-end
